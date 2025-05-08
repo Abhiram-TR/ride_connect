61644827ec66fccad9d18c239d9693c297d6c82f
Abhiram-TR <kannanabi377@gmail.com>
2025-05-08 17:41:47 +0530

    path c

--- ride_project/ride_project/settings.py
+++ ride_project/ride_project/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['abhiramt14.pythonanywhere.com', 'localhost', '127.0.0.1']
 
